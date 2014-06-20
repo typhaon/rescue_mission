d4091f036900311e60ffd0b41657619f531eb09a
Typhaon <davidgodwin788@gmail.com>
2014-06-20 15:06:16 -0400

    More methods and a view.
    Completed create and show methods in the questions controller.  Added
    show view.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,13 +7,29 @@ def index
 end
 
 def show
+  @question = Question.find(params[:id])
 end
 
 def new
   @question = Question.new
 end
 
+
 def create
+  @question = Question.new(question_params)
+  if @question.save
+    flash[:notice] = "Success!"
+    redirect_to questions_path
+  else
+    flash.now[:notice] = "Error!"
+    render :new
+  end
+end
+
+private
+
+def question_params
+  params.require(:question).permit(:title, :description)
 end
 
 end
