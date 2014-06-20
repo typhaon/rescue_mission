631ef54dc603cc2716bef199b3e63e7b8c0f59cc
Typhaon <davidgodwin788@gmail.com>
2014-06-20 14:12:51 -0400

    Updated Controller, Model, and Routes
    Created barebones controller.  Added model validations.
    Updated routes to only view, show, new and create.

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+
+def index
+end
+
+def show
+end
+
+def new
+end
+
+def create
+end
+
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ActiveRecord::Base
+    validates :title, prescence: true, length: { minimum: 40}
+    validates :description, prescence: true. length: { minimum: 150}
 end
